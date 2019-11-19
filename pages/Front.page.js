638f6bf060b613dbdfa107b8904aba098c9568a7
amish1188 <martin.joodd@gmail.com>
2019-11-19 19:01:21 +0100

    add button

--- pages/Front.page.js
+++ pages/Front.page.js
@@ -1,12 +1,14 @@
 const FrontPage = {
     template: `
-
-        <div><router-link to=login>Logg inn</router-link></div>
-        <div class="heroImage">
-            <h1>{{title}}</h1>
-        </div>
-        <div class="container">
-        <h1>Hello fuckers</h1>
+        <div>
+            <div><router-link to=login>Logg inn</router-link></div>
+            <div class="heroImage">
+                <h1>{{title}}</h1>
+                <button>Bestill time</button>
+            </div>
+            <div class="container">
+            <h1>Hello fuckers</h1>
+            </div>
         </div>
         
         
