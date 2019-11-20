0310603d16884913168f8ce05130dec28a197d1e
iaffs <byiaffs@gmail.com>
2019-11-20 15:26:00 +0100

    adds styling to login

--- components/LoginForm.component.js
+++ components/LoginForm.component.js
@@ -4,14 +4,14 @@ export default Vue.component('user-login-form',{
     template: ` 
     <div id="login">
         <form>
-            <h1>Login:</h1>
+            <h1 class="">Login:</h1>
             <p>{{info}}</p>
             <input type="text" name="email" v-model="email" placeholder="E-post Adresse"></input>
             <input type="password" name="password" v-model="password" placeholder="Passord"></input>
             <button type="button" v-on:click="loginWith()">Logg inn</button>
         </form>
 
-        <div>
+        <div class="example-users">
             <p>Eksempel Bruker: bruker@epost.no - Test123</p>
             <p>Eksempel Ansatt 1: ansatt1@clinic.no - Osteopop</p>
             <p>Eksempel Ansatt 2: ansatt2@clinic.no - ProStabber1337</p>
@@ -30,7 +30,7 @@ export default Vue.component('user-login-form',{
     methods:{
         loginWith(){
             console.log("Attempted login with: " + this.email + " - " + this.password)
-            if(!login(this.email, this.password)){
+            if (!login(this.email, this.password)) {
                 this.info = "Feil Passord eller Brukernavn!";
             }
         }
