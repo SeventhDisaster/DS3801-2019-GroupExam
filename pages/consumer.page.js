9066d7b4db4756f23e77387f492a1a7aaf1a0f3a
Tommyhama95 <tommyhamarsnes@gmail.com>
2019-11-20 16:22:14 +0100

    In progress - Consumer page

--- pages/consumer.page.js
+++ pages/consumer.page.js
@@ -3,7 +3,6 @@ import {confirmLogin, logout, getCurrentUser, getAppointments, appointments} fro
 const ConsumerPage = {
     template: `
         <div>
-
             <header class="header-consumer-m">
                 <h3 class="username-consumer-m">{{name}}</h3>
                 <button class="button-header-consumer-m button" v-on:click="userLoggingOut()">Logg ut</button>
@@ -11,37 +10,52 @@ const ConsumerPage = {
             <div>
                 <h1>{{title}}</h1>
             </div>
-
             <div class="booking-container-consumer-m">
-                <div class="upcoming-container-m" v->{{appointmentBox.type}}</div>
-
+                <div class="upcoming-container-m">
+                    <template v-for="item of appointments">
+                        <div class="apmtbox-consumer-m">
+                            <div class="apmtbox-type-consumer-m">{{item.type}}</div>
+                            <div class="apmtbox-date-consumer-m">Dato: {{item.date}}</div>
+                            <div class="apmtbox-time-consumer-m">Tid: {{item.time}}</div>
+                            <button :id="item.id" class="cancelbtn-consumer-m" v-on:click="overLayConsumer()">X</button>
+                        </div>
+                    </template>
+                </div>
             </div>
         </div>`,
     data() {
         return {
             title: "Dine kommende bookinger",
             name: "", 
-            appointmentBox: {
+            appointmentBox: { //Can be used if needed
                 type: "Kake",
                 date: "Lalala",
                 time: "2324"
             },
-            appointments: []
+            appointments: appointments //has the arrays
         }
     },
     mounted() {
         confirmLogin(); //Makes sure the user is logged in in order to access this site.
         let userObject = getCurrentUser();
         this.name = userObject.name;
-        let appointments = getAppointments();
-        this.listOutAppointments();
+        this.sortDate();
+        
     },
     methods: {
         userLoggingOut() {
             logout();
         }, 
-        listOutAppointments() {
-            this.appointmentBox.type = appointments[0].type;
+        sortDate() {
+            for(let i = 0; i < appointments.length; i++){
+                const date = appointments[i].date.split(".").reverse();
+                const finalDate = date[0]+"."+date[1]+"."+date[2];
+                appointments[i].date = finalDate;
+            }
+            console.log(appointments);
+        },
+        overLayConsumer() {
+            
         }
     }
 }
