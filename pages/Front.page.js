a47785255626e49d6ce65cd97625f99e9559d76b
amish1188 <martin.joodd@gmail.com>
2019-11-20 18:43:53 +0100

    add more style

--- pages/Front.page.js
+++ pages/Front.page.js
@@ -19,34 +19,38 @@ const FrontPage = {
                 <h2 class="intro">Tjenester</h2>
             </div>
             <div class="group section row">    
-                <div class="col span_1_of_4">
+                <div class="col span_1_of_4 test">
                     <h3>Osteopati</h3>
-                    <p>Akupunktur er en persontilpasset behandling der diagnostikk, behandling og oppfølging er skreddersydd for hver pasient med sine unike særtrekk og behov under hvert trinn av behandlingen.</p>
+                    <p class="feature-p">Akupunktur er en persontilpasset behandling der diagnostikk, behandling og oppfølging er skreddersydd for hver pasient med sine unike særtrekk og behov under hvert trinn av behandlingen.</p>
+                    <a href>Les mer</a>
                 </div>
-                <div class="col span_1_of_4">
+                <div class="col span_1_of_4 test">
                     <h3>Akupunktur</h3>
-                    <p>En osteopat undersøker og behandler muskel- og skjelettsmerter med manuelle behandlingsteknikker på benk. Terapeuten fokuserer på å avdekke funksjons- og bevegelsesforstyrrelser som kan være med på å skape smerte. Behandlingen egner seg for pasienter i alle aldere.</p>
+                    <p class="feature-p">En osteopat undersøker og behandler muskel- og skjelettsmerter med manuelle behandlingsteknikker på benk. Terapeuten fokuserer på å avdekke funksjons- og bevegelsesforstyrrelser som kan være med på å skape smerte. </p>
+                    <a href>Les mer</a>
                 </div>
-                <div class="col span_1_of_4">
+                <div class="col span_1_of_4 box">
                     <h3>Kostholdveiledning</h3>
-                    <p>Veiledning passer ypperlig for deg som ønsker å forebygge sykdom, gå ned eller opp i vekt, spise optimalt med tanke på trening og prestasjon, har allergier eller intoleranser som krever tilpassing av kosten eller ønsker å ha et generelt sunt kosthold.</p>
+                    <p class="feature-p">Veiledning passer ypperlig for deg som ønsker å forebygge sykdom, gå ned eller opp i vekt, spise optimalt med tanke på trening og prestasjon, har allergier eller intoleranser som krever tilpassing av kosten.</p>
                 </div>
-                <div class="col span_1_of_4">
+                <div class="col span_1_of_4 box">
                     <h3>Fysiologisk testlab</h3>
-                    <p>Testing av egen helse er samtidig både motiverende og gir verdifull informasjon for mange andre. Derfor tilbyr vi tester til deg som ønsker å teste din fysiske tilstand og prestasjonsevne.</p>
+                    <p class="feature-p">Testing av egen helse er samtidig både motiverende og gir verdifull informasjon for mange andre. Derfor tilbyr vi tester til deg som ønsker å teste din fysiske tilstand og prestasjonsevne.</p>
                 </div>
             </div>
         </section>
         <section class="opening-hours">
             <div class="group section row">
-                <div class="col span_1_of_2">
+                <div class="col span_1_of_2 box">
                     <img src="resources/group-satisfied-students-showing-thumbs-up_1098-2827.jpg">
                 </div>
                 <div class="col span_1_of_2">
                     <h2 class="intro">Priser</h2>
-                    <p>HK studenter får 50% rabatt hos oss!</p>
+                    <p class="hours-p">HK studenter får 50% rabatt hos oss!</p>
                 </div>
             </div>   
+        </section>
+        <section class="last-class">
             <div class="row">    
                 <h2 class="intro">Adresse</h2>
             </div>
