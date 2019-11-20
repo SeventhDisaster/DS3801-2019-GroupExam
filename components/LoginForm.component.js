a65ff0aa6b0e211a4dfdd64d749013c52e93c2dc
iaffs <byiaffs@gmail.com>
2019-11-20 22:15:16 +0100

    adds styling to employee.page.js

--- components/LoginForm.component.js
+++ components/LoginForm.component.js
@@ -2,23 +2,24 @@ import {login} from './Index.js';
 
 export default Vue.component('user-login-form',{
     template: ` 
-    <div id="login">
-        <form>
-            <h1 class="">Login:</h1>
-            <p>{{info}}</p>
-            <input type="text" name="email" v-model="email" placeholder="E-post Adresse"></input>
-            <input type="password" name="password" v-model="password" placeholder="Passord"></input>
-            <button type="button" v-on:click="loginWith()">Logg inn</button>
-        </form>
+    <div id="row">
+        <div class="col">
+            <form>
+                <h1 class="">Login:</h1>
+                <p>{{info}}</p>
+                <input type="text" name="email" v-model="email" placeholder="E-post Adresse"></input>
+                <input type="password" name="password" v-model="password" placeholder="Passord"></input>
+                <button type="button" v-on:click="loginWith()">Logg inn</button>
+            </form>
 
-        <div class="example-users">
-            <p>Eksempel Bruker: bruker@epost.no - Test123</p>
-            <p>Eksempel Ansatt 1: ansatt1@clinic.no - Osteopop</p>
-            <p>Eksempel Ansatt 2: ansatt2@clinic.no - ProStabber1337</p>
-            <p>Eksempel Ansatt 2: ansatt3@clinic.no - FastFud4Life</p>
-            <p>Eksempel Ansatt 2: ansatt4@clinic.no - 123456</p>
+            <div>
+                <p>Eksempel Bruker: bruker@epost.no - Test123</p>
+                <p>Eksempel Ansatt 1: ansatt1@clinic.no - Osteopop</p>
+                <p>Eksempel Ansatt 2: ansatt2@clinic.no - ProStabber1337</p>
+                <p>Eksempel Ansatt 2: ansatt3@clinic.no - FastFud4Life</p>
+                <p>Eksempel Ansatt 2: ansatt4@clinic.no - 123456</p>
+            </div>
         </div>
-
     </div>`,
     data: function() {
         return {
@@ -28,7 +29,7 @@ export default Vue.component('user-login-form',{
         }
     },
     methods:{
-        loginWith(){
+        loginWith() {
             console.log("Attempted login with: " + this.email + " - " + this.password)
             if (!login(this.email, this.password)) {
                 this.info = "Feil Passord eller Brukernavn!";
--- pages/employee.page.js
+++ pages/employee.page.js
@@ -2,16 +2,22 @@ import {confirmLogin, logout} from '../components/Index.js';
 
 const EmployeePage = {
     template: `
-        <div class="flex-container">
-            <h1>{{title}}</h1>
-                <div class="bookings-employee">
-                <div></div>
-                <div></div>
-                <div></div>
+        <div>
+            <div class="group section row">
+                <div class="col span_1_of_3 centered"></div>
+                        <h1 class="col span_1_of_3 centered">{{title}}</h1>
+                        <div class="col span_1_of_3 centered"></div>
                 </div>
-        <div class="something-else"></div>
-            <button class="button button-light button-dark-logout-employee" v-on:click="loggut()">Logg Ut</button>
-            <div></div>
+
+                <div class="group section row">
+                    <div class="col span_1_of_3"></div>
+                        <button class="button button-light centered col span_1_of_3" v-on:click="loggut()">Logg Ut</button>
+                        <div class="col span_1_of_3"></div>
+                </div>
+
+                <div class="group section row">
+                    <div class="col span_1_of_3"></div>
+                </div>
         </div>`,
     data() {
         return {
