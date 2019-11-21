3de639a1c3f78620d400091906cf4b25676f9a95
iaffs <byiaffs@gmail.com>
2019-11-21 15:02:59 +0100

    moved logout button

--- pages/employee.page.js
+++ pages/employee.page.js
@@ -1,19 +1,30 @@
-import {confirmLogin, logout} from '../components/Index.js';
+import {
+    confirmLogin,
+    logout
+} from '../components/Index.js';
 
 const EmployeePage = {
     template: `
         <div class="content-box">
             <div class="group section row">
-                <div class="col span_1_of_3 centered"></div>
-                    <h1 class="col span_1_of_3 centered">{{title}}</h1>
+                <div class="col span_1_of_5 centered"></div>
+                <div class="col span_1_of_5 centered"></div>
+                <div class="col span_1_of_5 centered"></div>
+                <div class="col span_1_of_5 centered"></div>
+                <button class="button-dark logout-employee left-float col span_1_of_5" v-on:click="loggut()">Logg Ut</button>
+        </div>
+            
+                <div class="group section row">
+                    <div class="col span_1_of_3 centered"></div>
+                    <h1 class="col span_1_of_3 centered employee-title">{{title}}</h1>
                     <div class="col span_1_of_3 centered"></div>
                 </div>
 
                 <div class="group section row">
-                <button class="col span_1_of_4 centered button-employee">Profil</button>
-                <button class="col span_1_of_4 centered button-employee">Dagens timer</button>
-                <button class="col span_1_of_4 centered button-employee">Mailboks</button>
-                <button class="col span_1_of_4 centered button-employee">Klienter</button>
+                    <button class="col span_1_of_4 centered button-employee">Profil</button>
+                    <button class="col span_1_of_4 centered button-employee">Dagens timer</button>
+                    <button class="col span_1_of_4 centered button-employee">Mailboks</button>
+                    <button class="col span_1_of_4 centered button-employee">Klienter</button>
                 </div>
 
 
@@ -86,8 +97,8 @@ const EmployeePage = {
 
                 <div class="group section row">
                     <div class="col span_1_of_3"></div>
-                        <button class="button button-dark centered col span_1_of_3" v-on:click="loggut()">Logg Ut</button>
-                        <div class="col span_1_of_3"></div>
+                    <div class="col span_1_of_3"></div>
+                    <div class="col span_1_of_3"></div>
                 </div>
 
                 <div class="group section row">
@@ -103,7 +114,7 @@ const EmployeePage = {
         confirmLogin(); //Makes sure the user is logged in in order to access this site.
     },
     methods: {
-        loggut(){
+        loggut() {
             logout();
         }
     }
