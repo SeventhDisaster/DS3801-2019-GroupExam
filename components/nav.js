1015189b32ec08e978638f6012136dccd72208f2
amish1188 <martin.joodd@gmail.com>
2019-11-23 12:42:21 +0100

    add js to nav

---
+++ components/nav.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    //nav toggle
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+    
+        //animated links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.3 }s`;
+            }
+        });
+        //burger toggle
+        burger.classList.toggle("toggle");
+    });
+  
+}
+
+navSlide();
