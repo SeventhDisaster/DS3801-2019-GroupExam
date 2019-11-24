06b355a8d5076f76d6c60acbf43cc40e54bc3ad7
iaffs <byiaffs@gmail.com>
2019-11-24 17:15:08 +0100

    adds dagens timer in em.today.page.js

--- pages/Emp.today.page.js
+++ pages/Emp.today.page.js
@@ -1,11 +1,30 @@
+import * as components from '../components/Index.js';
+import { getCurrentUser } from '../components/Index.js';
+
+components.appointments
+
 const EmployeeTodayPage = {
     template: `
         <div>
             <h1>{{title}}</h1>
+            <div v-for="myAppointment in myAppointments">
+                {{myAppointment.date}} - {{myAppointment.time}}
+            </div>
         </div>`,
     data() {
         return {
-            title: "Employee day schedule page"
+            title: "Employee day schedule page",
+            myAppointments: []
+        }
+    },
+    mounted() {
+    let user = components.getCurrentUser();
+    let field = user.inField;
+
+        for (let appointment of components.appointments) {
+            if (appointment.type == field) {
+            this.myAppointments.push(appointment)
+            }
         }
     }
 }
