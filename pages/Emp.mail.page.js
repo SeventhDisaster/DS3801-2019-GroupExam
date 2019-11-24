c3400827ba635bd921d2b71e3a7021093e790d96
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-11-24 20:14:51 +0100

    Added routing to profile and mail

---
+++ pages/Emp.mail.page.js
@@ -0,0 +1,13 @@
+const EmployeeMailPage = {
+    template: `
+        <div>
+            <h1>{{title}}</h1>
+        </div>`,
+    data() {
+        return {
+            title: "Employee mail page"
+        }
+    }
+}
+
+export default EmployeeMailPage;
---
+++ pages/Emp.profile.page.js
@@ -0,0 +1,13 @@
+const EmployeeProfilePage = {
+    template: `
+        <div>
+            <h1>{{title}}</h1>
+        </div>`,
+    data() {
+        return {
+            title: "Employee Profile Page"
+        }
+    }
+}
+
+export default EmployeeProfilePage;
