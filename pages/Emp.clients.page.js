3f398ec7fef2ae0aeaf5e225f85e28906cd0adbc
iaffs <byiaffs@gmail.com>
2019-11-24 20:01:38 +0100

    adds styling to e,ployee client page

--- pages/Emp.clients.page.js
+++ pages/Emp.clients.page.js
@@ -2,12 +2,19 @@ import * as components from '../components/index.js'
 
 const EmployeeClientsPage = {
     template: `
-        <div>
-            <h1>{{title}}</h1>
-            <div v-for="client in clients" :key="client.email">
-                <client-list-element :user="client"></client-list-element>
+    <div class="content-box">
+        <div class="group section row">
+            <div class="col span_1_of_3"></div>
+            <h1 class="col span_1_of_3">{{title}}</h1>
+            <div class="col span_1_of_3"></div>
+
+            <div class="group section row">
+                <div v-for="client in clients" :key="client.email">
+                    <client-list-element :user="client"></client-list-element>
+                </div>
             </div>
-        </div>`,
+        </div>
+    </div>`,
     data() {
         return {
             title: "Employee client page",
@@ -15,8 +22,8 @@ const EmployeeClientsPage = {
         }
     },
     mounted() {
-        for(let user of components.users){
-            if(!user.isEmployed){
+        for(let user of components.users) {
+            if (!user.isEmployed) {
                 this.clients.push(user);
             }
         }
