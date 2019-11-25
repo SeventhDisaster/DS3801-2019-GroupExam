c3e495c46300b7c2d27056c5076a1c837461416d
iaffs <byiaffs@gmail.com>
2019-11-25 13:58:11 +0100

    adds style to employee client page

--- components/Emp.clients.list.component.js
+++ components/Emp.clients.list.component.js
@@ -2,21 +2,41 @@ import * as components from './index.js';
 
 export default Vue.component('client-list-element', {
     template: `
-    <div>
-        <label>{{user.name}}    
-            <input type="checkbox" v-model="isActive">
-        </label>
-        <div v-show="isActive">
-            <ul>
-                <li v-for="(note, index) in user.notes">
-                    <input @keyup="setNoteChange(user.email, index, $event.target.value)" :value="note">
-                    <button @click="deleteNote(user.email, index, note)">Delete Note</button>
-                </li>
-            </ul>
-            <form>
-                <textarea v-model="newNote" placeholder="Skriv nytt notat her" @keyup.enter="addNote(user.email)"></textarea>
-                <button class="" action="submit" @click="addNote(user.email)">Add new note</button>
-            </form>
+    <div class="group section row">
+        <div class="col span_1_of_2 user-info-box">
+
+            <div class="group section row">
+                <div class="centered">
+                    <label class="user-info-label">{{user.name}}    
+                        <input type="checkbox" style="display:none" v-model="isActive">
+                    </label>
+                
+
+                <div v-show="isActive">
+                    <ul class="list-style">
+                        <li v-for="(note, index) in user.notes">
+                            <input class="note-input centered" @keyup="setNoteChange(user.email, index, $event.target.value)" :value="note">
+                            <button class="button button-light delete-button" @click="deleteNote(user.email, index, note)">Slett</button>
+                        </li>
+                    </ul>
+
+                    <form class="new-note-form group section row">
+                        <div class="col span_1_of_3 centered"></div>
+                            <textarea class="col span_3_of_3 centered form-text-area" v-model="newNote" placeholder="Skriv nytt notat her" @keyup.enter="addNote(user.email)"></textarea>
+                        <div class="col span_1_of_3 centered"></div>
+
+                    <div class="group section row">
+                        <div class="col span_1_of_3"></div>
+                            <button class="button button-light new-note-button centered col span_1_of_3" action="submit" @click="addNote(user.email)">Nytt notat</button>
+                        <div class="col span_1_of_3"></div>
+                    </div>
+                    </form>
+                </div>
+
+                </div>
+
+            </div>
+
         </div>
     </div>
     `,
@@ -46,8 +66,8 @@ export default Vue.component('client-list-element', {
             }
         },
         addNote(userEmail) {
-            for(let user of components.users) {
-                if(user.email == userEmail){
+            for (let user of components.users) {
+                if (user.email == userEmail) {
                     user.notes.push(this.newNote);
                     this.newNote = "";
                 }
