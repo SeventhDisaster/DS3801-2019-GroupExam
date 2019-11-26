1e7aa6a41fc8456a9561d4b0fe13ad279e5bd6f8
amish1188 <martin.joodd@gmail.com>
2019-11-26 22:41:07 +0100

    styling login page

--- components/LoginForm.component.js
+++ components/LoginForm.component.js
@@ -2,39 +2,14 @@ import {login} from './Index.js';
 
 export default Vue.component('user-login-form',{
     template: `
-    <div class="content-box">
-
-        <div id="group section row">
-            <div class="col span_1_of_5 centered"></div>
-                <form class="col span_3_of_5 centered">
-                    <h1 class="centered">Logg inn:</h1>
+    <div>
+        <form class="signup-form">
+                    <h2>Logg inn:</h2>
                     <p>{{info}}</p>
-                    <input type="text" class="input-form" name="email" v-model="email" placeholder="E-post adresse"/>
-                    <input type="password" class="input-form" name="password" v-model="password" placeholder="Passord" @keyup.enter="loginWith()"/>
-                </form>
-                <div class="col span_1_of_5"></div>
-        </div>
-
-        <div class="group section row">
-        <div class="col span_1_of_3"></div>
-        <button type="button" class="button button-light login-button centered col span_1_of_3" v-on:click="loginWith()">Logg inn</button>
-        <div class="col span_1_of_3"></div>
-        
-        </div>
-
-        <div class="group section row">
-            <div class="col span_1_of_3"></div>
-            <div class="col span_1_of_3">
-                <p>Eksempel Bruker: brukermann@epost.no - Test123</p>
-                <p>Eksempel Ansatt 1: ansatt1@clinic.no - Osteopop</p>
-                <p>Eksempel Ansatt 2: ansatt2@clinic.no - ProStabber1337</p>
-                <p>Eksempel Ansatt 2: ansatt3@clinic.no - FastFud4Life</p>
-                <p>Eksempel Ansatt 2: ansatt4@clinic.no - 123456</p>
-            </div>
-            <div class="col span_1_of_3"></div>
-        </div>
-
-        </div>
+                    <input class="signup-input" type="text"  name="email" v-model="email" placeholder="E-post adresse"/>
+                    <input class="signup-input" type="password"  name="password" v-model="password" placeholder="Passord" @keyup.enter="loginWith()"/>
+                    <button type="button" class="login-btn" v-on:click="loginWith()">Logg inn</button>
+        </form>
     </div>`,
     data: function() {
         return {
--- pages/Login.page.js
+++ pages/Login.page.js
@@ -1,9 +1,44 @@
 const LoginPage = {
     template: `
-        <div class="content-box">
-            <user-login-form></user-login-form>
-            <router-link style="color: red" to=register>Registrer Deg</router-link>
+        <div>
+            <div class="group section">
+                <div class="col-margin span_1_of_2 picture-half">
+                    <div class="login-picture">
+                        <div class="signup-logo">
+                            <img class="signup-logo-kristiania" src="./resources/KristianiaUniversityCollege_hvit_rgb.png">
+                            <h2>Studentklinikken</h2>
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-margin span_1_of_2 form-half">
+                        <div class="signup-form">
+                            <user-login-form></user-login-form>
+                            <router-link to=register class="register-login">Registrer Deg</router-link>
+                            <p>Eksempel Bruker: brukermann@epost.no - Test123</p>
+                            <p>Eksempel Ansatt 1: ansatt1@clinic.no - Osteopop</p>
+                        </div>
+                </div>  
+            </div>
         </div>`
 };
 
 export default LoginPage;
+
+/*
+<div class="group section">
+                <div class="col-margin span_1_of_2 picture-half">
+                    <div class="signup-picture">
+                        <div class="signup-logo">
+                            <img class="signup-logo-kristiania" src="./resources/KristianiaUniversityCollege_hvit_rgb.png">
+                            <h2>Studentklinikken</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-margin span_1_of_2 form-half">
+                    <div class="signup-form">
+                        <user-register-form></user-register-form>
+                        <router-link to=login class="signup-login">Jeg er allerede bruker - Logg Inn</router-link>
+                    </div>
+                </div>
+            </div>
+*/
