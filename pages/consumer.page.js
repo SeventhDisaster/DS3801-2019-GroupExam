e464f57fa838283d2aab13310173feb7b053b9a9
Tommyhama95 <tommyhamarsnes@gmail.com>
2019-11-26 16:59:10 +0100

    Consumer page loading bookings correctly

--- pages/consumer.page.js
+++ pages/consumer.page.js
@@ -1,4 +1,4 @@
-import {confirmLogin, logout, getCurrentUser, getAppointments, appointments} from '../components/index.js'
+import {confirmLogin, logout, getCurrentUser, getAppointments, appointments, updateAll} from '../components/index.js'
 
 const ConsumerPage = {
     template: `
@@ -10,13 +10,13 @@ const ConsumerPage = {
                     <button class="button-header-consumer-m button" @click="userLoggingOut()">Logg ut</button>
                 </header>
                 <div>
-                    <h1>{{title}}</h1>
+                    <h1 class="consumer-title-m">{{title}}</h1>
                 </div>
                 <div class="booking-container-consumer-m">
                     <div class="upcoming-container-m">
                         <!-- Upcoming bookings -->
-                        <div v-if="userAppointments.length != 0"> <!-- Run if user has appointments -->
-                            <template v-for="item of userAppointments">
+                        <div v-if="myAppointments.length != 0"> <!-- Run if user has appointments -->
+                            <template v-for="item of myAppointments">
                                 <div class="apmtbox-consumer-m">
                                     <div class="apmtbox-type-consumer-m">{{item.type}}</div>
                                     <div class="apmtbox-date-value-consumer-m">{{item.date}}</div>
@@ -29,22 +29,24 @@ const ConsumerPage = {
                             <h4>{{noAppointmentsText}}</h4>
                         </div>
                         </div>
-                        <div v-if="oldAppointments.length != 0">
-                            <!-- Split between upcoming and previous bookings -->
-                            <div class="previous-split-container-consumer-m">
-                                <div class="splitline-consumer-m"></div>
-                                <h4 class="previous-title-consumer-m">Tidligere bookinger</h4>
-                            </div>
-                            <!-- Previous bookings -->
-                                <div class="previus-bookings-consumer-m">
-                                    <template v-for="preBooking of oldAppointments">
-                                        <div class="apmtbox-consumer-m previous-consumer-m">
-                                            <div class="apmtbox-type-consumer-m">{{preBooking.type}}</div>
-                                            <div class="apmtbox-date-value-consumer-m">{{preBooking.date}}</div>
-                                            <div class="apmtbox-time-value-consumer-m"> {{preBooking.time}}</div>
-                                        </div>
-                                    </template>
+                        <div class="oldAppointments-container-m">
+                            <div v-if="oldAppointments.length != 0">
+                                <!-- Split between upcoming and previous bookings -->
+                                <div class="previous-split-container-consumer-m">
+                                    <div class="splitline-consumer-m"></div>
+                                    <h4 class="previous-title-consumer-m">Tidligere bookinger</h4>
                                 </div>
+                                <!-- Previous bookings -->
+                                    <div class="previus-bookings-consumer-m">
+                                        <template v-for="preBooking of oldAppointments">
+                                            <div class="apmtbox-consumer-m previous-consumer-m">
+                                                <div class="apmtbox-type-consumer-m">{{preBooking.type}}</div>
+                                                <div class="apmtbox-date-value-consumer-m">{{preBooking.date}}</div>
+                                                <div class="apmtbox-time-value-consumer-m"> {{preBooking.time}}</div>
+                                            </div>
+                                        </template>
+                                    </div>
+                            </div>
                         </div>
                 </div>
             </div>
@@ -55,7 +57,8 @@ const ConsumerPage = {
             name: "", 
             noAppointmentsText: "Du har ingen bookinger.",
             previousText: "",
-            userAppointments: [],
+            myAppointments: [],
+            appointHolder: [],
             oldAppointments: [],
             visible: false, 
             id: ""
@@ -63,52 +66,65 @@ const ConsumerPage = {
     },
     mounted() {
         confirmLogin(); //Makes sure the user is logged in in order to access this site.
+        updateAll();
         let userObject = getCurrentUser(); //Gets the logged in user
         this.name = userObject.name;
         this.assignAppointments(userObject); //Gets the correct appointments of user
-        this.sortDate(); //Parses date format from "YYYY-MM-DD" => "DD-MM-YYYY"
-    },
+        this.sortAppointmentsByDate();
+        this.sortArrayByDate(this.myAppointments);
+        this.sortArrayByDate(this.oldAppointments);
+        this.oldAppointments.reverse();
+        this.parseDate(this.myAppointments); //Parses date format from "YYYY-MM-DD" => "DD-MM-YYYY"
+        this.parseDate(this.oldAppointments); //Parses date format from "YYYY-MM-DD" => "DD-MM-YYYY"
+    }, 
     methods: {
         userLoggingOut() {
             logout();
         },
         assignAppointments(userObject) {
-            let arr = [];
-
+            let appointments = getAppointments();
+            
             // Assigning correct user appointments
             for(let i = 0; i < appointments.length; i++) {
                 for(let j = 0; j < userObject.appointmentIds.length; j++) {
                     if(appointments[i].id === userObject.appointmentIds[j]) {
                         const item = appointments[i];
-                        this.userAppointments.push(item);
+                        this.appointHolder.push(item);
                     } else { /*do nothing*/ }
                 }
             }
-            
+        },
+        sortAppointmentsByDate() {
             // sorting by date
-            const arrayLength = this.userAppointments.length;
-            let uaArray = this.userAppointments;
+            let uaArray = this.appointHolder;
+            let comingArray = [];
+            let pastArray = [];
             let today = "2019-11-22"; //Value checked for bookings already happened
-
-            for(let n = 0; n < arrayLength; n++) {
-                //Checks for appointments before todays date
-                if(uaArray[n].date <= today) {
-                    this.oldAppointments.push(uaArray[n]);
-                    this.userAppointments.splice(n, 1);
+            
+            for(let i = 0; i < uaArray.length; i++) {
+                if(uaArray[i].date < today) {
+                    const item = uaArray[i];
+                    pastArray.push(item);
                 } else {
-                    //Assigns if appointment has not been yet
-                    for(let m = 0; m < arrayLength; m++) {
-                        if(uaArray[n].date < uaArray[m].date) {
-                        let a = uaArray[m];
-                        uaArray[m] = uaArray[n];
-                        uaArray[n] = a;
-                        } else { /*do nothing*/ }
-                    }
+                    const item = uaArray[i];
+                    comingArray.push(item);
                 }
             }
-            this.userAppointments = uaArray;
+            this.myAppointments = comingArray;
+            this.oldAppointments = pastArray;
         },
-        sortDate() {
+        sortArrayByDate(inArray) {
+            for(let i = 0; i < inArray.length; i++) {
+                for(let n = 1; n <= inArray.length; n++) {
+                    if(inArray[i] > inArray[n]) {
+                        let a = inArray[n];
+                        inArray[n] = inArray[i];
+                        inArray[i] = a;
+                    } else {/* Do nothing */}
+                }
+            }
+        },
+        parseDate(appointments) {
             //Reordering date format for readability
             for(let i = 0; i < appointments.length; i++){
                 const date = appointments[i].date.split("-").reverse();
@@ -124,12 +140,12 @@ const ConsumerPage = {
             this.visible = false;
         }, 
         deleteAppointment() {
-            let array = this.userAppointments;
+            let array = this.myAppointments;
             
             for(let item of array) {
                 if(item.id === this.id) {
                     const index = array.indexOf(item);
-                    this.userAppointments.splice(index, 1); 
+                    this.myAppointments.splice(index, 1); 
                 }
             }
         }
