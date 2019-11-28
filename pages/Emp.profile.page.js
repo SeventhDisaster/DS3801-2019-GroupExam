8e766de24c7bf53f18361c702b18c096a92733cc
iaffs <byiaffs@gmail.com>
2019-11-28 19:09:28 +0100

    added final style to employee profile page

--- pages/Emp.profile.page.js
+++ pages/Emp.profile.page.js
@@ -3,31 +3,58 @@ import { router } from '../router.js';
 
 const EmployeeProfilePage = {
     template: `
-        <div>
-            <div class="content-box">
-                <div class="employee-profile-page">
-                    <button class="col span_1_of_9 button-dark button back-button" @click = "back">Tilbake</button>
-                    <h1 class="employee-profile-title">{{title}}</h1>
-                    <div class="employee-profile-container">
-                        <div class="employee-profile-name">Ditt registrerte navn: 
-                            <div>{{name}}</div>
-                        </div>
-                        <br>
-                        <div class="employee-profile-email">Din registrerte e-mail: 
-                            <div>{{email}}</div>
-                        </div>
-                        <br>
-                        <div class="employee-profile-phone">Ditt registrerte telefon nr: 
-                            <div>{{phone}}</div>
-                        </div>
-                        <img src="../resources/sampleuser.png" class="employee-profile-picture">
-                        <br>
-                        <br>
-                        <div class="employee-profile-message">Om din informasjon skulle være feil, vennligst kontakt skolens system ansvarlig for endringer eller din foreleser for å sende informasjonen videre.</div>
-                    </div>
-                </div>
-            </div>
-        </div>`,
+
+    <div class="content-box">
+
+        <div class="group section row">
+            <div class="col span_1_of_9"><button class="button-dark button back-button back-button-employee" @click = "back">Tilbake</button></div>
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>            
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>
+            <div class="col span_1_of_9"></div>
+        </div>
+
+        <div class="group section row">
+            <div class="col span_1_of_3"></div>
+            <div class="col span_1_of_3 centered"><h1 class="employee-profile-title">{{title}}</h1></div>
+            <div class="col span_1_of_3"></div>
+        </div>
+
+        <div class="group section row">
+            <div class="col span_1_of_6"></div>
+            <div class="col span_1_of_6"></div>
+            <div class="col span_2_of_6"></div>
+            <div class="col span_1_of_6"></div>
+            <div class="col span_1_of_6"><img src="../resources/sampleuser.png" class="employee-profile-picture"></div>
+        </div>
+
+        <br>
+
+        <div class="group section row centered">
+            <div class="col span_1_of_5"></div>
+            <div class="col span_3_of_5 centered styling-client-information">
+                        Ditt registrerte navn: {{name}}
+                    <br>Din registrerte e-mail: {{email}}
+                    <br>Ditt registrerte telefon nr: {{phone}}</div>
+                <br>
+                <div class="col span_1_of_5"></div>
+        </div>
+        <br>
+        <br>
+
+        <div class="group section row">
+            <div class="col span_1_of_3"></div>
+            <div class="col span_1_of_3 centered">Om din informasjon skulle være feil, vennligst kontakt skolens system ansvarlig for endringer eller din foreleser for å sende informasjonen videre.</div>
+            <div class="col span_1_of_3"></div>
+        </div>
+
+
+    </div>
+        `,
     data() {
         return {
             title: "Ansatt profil",
